f51b3fad133363b474bdca237a46a30c182471bf
justin <justin@justin.net>
2022-04-10 19:21:57 -0400

    Initial working Gatys et al. style transfer.

--- deepdream.py
+++ deepdream.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 
+"""
+Requires pytorch 1.9.0+.  This is the first version of pytorch where the torch.clamp() function accepts tensors instead
+of scalars for the min/max values.  This enables per-RGB-channel min/max clamping which helps produce higher-quality
+non-saturated images.  The use of per-channel min/max is desirable because the images are normalized using distinct 
+mean and standard deviation per-channel.  This result is each channel has different mins/maxs.
+"""
+
 import torch
 import random
 
@@ -12,12 +19,15 @@ from code import interact
 MEAN = torch.tensor([0.485, 0.456, 0.406])
 STD  = torch.tensor([0.229, 0.224, 0.225])
 
+
+
 def main():
     #model = models.vgg19(pretrained=True).to("cuda")
-    model = models.googlenet(pretrained=True).to("cuda")
+    #model = models.googlenet(pretrained=True).to("cuda")
+    model = models.convnext_base(pretrained=True).to("cuda")
     print(model)
     
-    deepdream("content_images/clouds.jpeg", model.inception4e, model, "clouds_normed", step_size=15, n_iters=50, octave_scale=1.4, n_octave=4)
+    #deepdream("content_images/clouds.jpeg", model.inception4e, model, "clouds_normed", step_size=15, n_iters=50, octave_scale=1.4, n_octave=4)
     #deepdream("content_images/beksinski_1.jpg", 36, model, "beksinski_1", step_size=5, n_iters=100)
     #deepdream("content_images/beksinski_2.jpg", 36, model, "beksinski_2", step_size=5, n_iters=100)
     #deepdream("content_images/beksinski_3.jpg", 36, model, "beksinski_3", step_size=5, n_iters=100, octave_scale=1.2, n_octave=10)
@@ -27,17 +37,87 @@ def main():
     #deepdream("content_images/beksinski_6.jpg", 36, model, "beksinski_6", step_size=5, n_iters=50, octave_scale=1.4, n_octave=10)
     #deepdream("content_images/waterfall.jpg", 36, model, "waterfall", step_size=5, n_iters=50, octave_scale=1.4, n_octave=10)
     #deepdream("content_images/4k_skull.jpg", 36, model, "4k_skull", step_size=5, n_iters=100, octave_scale=1.1, n_octave=16)
-    exit()
+    #exit()
 
-    for target_layer in range(len(model.features)):
-        if isinstance(model.features[target_layer], (torch.nn.modules.pooling.MaxPool2d, torch.nn.modules.activation.ReLU)):
-            print("maximizing activations for layer %d:  %s\n" % (target_layer, str(model.features[target_layer])))
-            deepdream("content_images/clouds.jpeg", target_layer, model, "clouds")
+    #enumerate convnext layers for style transfer
+    for idx in range(7, 0, -1):
+        target_layer = model.features[idx]
+        #register forward hook to extract layer activations
+        #adapted from:  https://discuss.pytorch.org/t/how-can-i-extract-intermediate-layer-output-from-loaded-cnn-model/77301
+        activations = {}
+        
+        def get_activation(name):
+            def hook(model, input, output):
+                activations[name] = output
+            return hook
+        target_layer.register_forward_hook(get_activation(target_layer))
+        model.forward(preprocess(Image.open("style_images/flowers.jpeg")).unsqueeze(0).cuda())
+        model.eval()
+        target_activations = activations[target_layer]
 
-def deepdream(in_filename, #input image filename
+    #    interact(local=locals())
+        def style_objective(x):
+            x = x.squeeze().flatten(1)
+            y = target_activations.clone().detach()#.requires_grad_(True)
+            y = y.squeeze().flatten(1)
+            a = x.T @ y
+            a = y[:, a.argmax(1)]
+            a = a / x
+
+            #a = torch.linalg.norm(a)
+            #interact(local=locals())
+            #a = a.sum() 
+            return a
+        
+
+        deepdream("content_images/clouds.jpeg", target_layer, style_objective, model, "convnext_base/clouds_layer_%d" % idx, step_size=15, n_iters=100, octave_scale=1.4, n_octave=4)
+
+
+#    #enumerate VGG layers for style transfer
+#    for idx in range(36, 8, -1):
+#        target_layer = model.features[idx]
+#        #register forward hook to extract layer activations
+#        #adapted from:  https://discuss.pytorch.org/t/how-can-i-extract-intermediate-layer-output-from-loaded-cnn-model/77301
+#        activations = {}
+#        
+#        def get_activation(name):
+#            def hook(model, input, output):
+#                activations[name] = output
+#            return hook
+#        target_layer.register_forward_hook(get_activation(target_layer))
+#        model.forward(preprocess(Image.open("style_images/flowers.jpeg")).unsqueeze(0).cuda())
+#        model.eval()
+#        target_activations = activations[target_layer]
+#
+#    #    interact(local=locals())
+#        def style_objective(x):
+#            x = x.squeeze().flatten(1)
+#            y = target_activations.clone().detach()#.requires_grad_(True)
+#            y = y.squeeze().flatten(1)
+#            a = x.T @ y
+#            a = y[:, a.argmax(1)]
+#            a = a / x
+#
+#            #a = torch.linalg.norm(a)
+#            #interact(local=locals())
+#            #a = a.sum() 
+#            return a
+#        
+#
+#        deepdream("content_images/clouds.jpeg", target_layer, style_objective, model, "clouds_layer_%d" % idx, step_size=15, n_iters=100, octave_scale=1.4, n_octave=4)
+    #idx=36
+    #deepdream("content_images/mat.jpg", model.fc, lambda x : torch.linalg.norm(x), model, "mat_layer_%d" % idx, step_size=10, n_iters=100, octave_scale=1.7, n_octave=4)
+    
+#    for idx, target_layer in enumerate(model.features):
+#        if isinstance(target_layer, (torch.nn.modules.pooling.MaxPool2d, torch.nn.modules.activation.ReLU)):
+#            print("maximizing activations for layer:  %s\n" % str(target_layer))
+#            deepdream("content_images/clouds.jpeg", target_layer, sum_objective_function, model, "clouds_layer_%d" % idx, step_size=5, n_iters=200, octave_scale=1.4, n_octave=4)
+
+def deepdream(in_filename,       #input image filename
               target_layer,      #a layer in the model which can have a forward hook registered
+              objective_function,#a lambda style objective function to apply to the activations of the target layer
               model,             #the pretrained CNN that will be used to generate the image
-              out_filename,  #output image filename
+              out_filename,      #output image filename
               step_size=6.0,     #higher learning rate BC we account for ImageNet STD
               n_iters=10,        #number of gradient ascent steps per octave
               jitter=32,         #max number of pixels to shift in x/y direction
@@ -46,16 +126,19 @@ def deepdream(in_filename, #input image filename
     """
     Based on Google Deep Dream:  https://github.com/google/deepdream/blob/master/dream.ipynb
     
-    Perform gradient ascent on an image to maximize the activations of a target layer.
+    Perform gradient ascent on an image to maximize the activations of a target layer after applying a objective function.
 
-    - n_iters:  Number of iterations per octave
-    - jitter:  M 
+    Example objective functions:
+    lambda x : x.sum() #sum all activations
+    lambda x : lambda x : torch.linalg.norm(x)) #take norm of all activations
+    lambda x : x[0, 65] #optimize for a particular class.  Class indexes:  https://deeplearning.cms.waikato.ac.nz/user-guide/class-maps/IMAGENET/
+    lambda x : x[0, [65, 66]].sum() #optimize for a number of classes
     """
 
-    #ensure tensor has 4 dimensions (N x C x H x W)
+    #expand tensor has 4 dimensions (N x C x H x W)
     base_img = preprocess(Image.open(in_filename)).unsqueeze(0).cuda()
 
-    #clamping values, shape:  (1, 3, 1, 1)
+    #per channel clamping values, shape:  (1, 3, 1, 1)
     mins = (-MEAN / STD).unsqueeze(0).unsqueeze(-1).unsqueeze(-1).cuda()
     maxs = ((1 - MEAN) / STD).unsqueeze(0).unsqueeze(-1).unsqueeze(-1).cuda()
 
@@ -99,20 +182,24 @@ def deepdream(in_filename, #input image filename
             #create backprop-able image
             img_var = img.clone().detach().requires_grad_(True)
 
-            #make forward pass and set loss function
+            #make forward pass and set objective function
             model.forward(img_var)
-            loss = activations[target_layer].sum()
-            #loss = torch.linalg.norm(activations[target_layer])
+            objective = objective_function(activations[target_layer])
 
             #backprop to the image
             model.zero_grad()
-            loss.backward()
+            a = activations[target_layer]
+            objective = objective.reshape(a.shape)
+            torch.autograd.backward(a, grad_tensors=objective)
+            #interact(local=locals())
+            #objective.backward()
 
             #normalize gradients
             g = img_var.grad
             g /= torch.linalg.norm(g) #norm is a deviation, original used abs().mean()
             
             #gradient step
+            #interact(local=locals())
             img = img + (step_size * g)
             
             #clamp min and max values
@@ -125,7 +212,7 @@ def deepdream(in_filename, #input image filename
         detail = img - octave
 
         #save the image on each octave
-        deprocess(img.squeeze(0).clone().cpu()).save("visualization/%s_iter_%d.png" % (out_filename, idx))
+        deprocess(img.squeeze(0).clone().cpu()).save("stylized_images/%s.png" % out_filename)
 
 def preprocess(img):
     convert   = transforms.ToTensor()
