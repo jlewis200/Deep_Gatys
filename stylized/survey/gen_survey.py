e26cce68a21aace3e06e9fc755547192b2ad52fd
justin <18606523+jlewis200@users.noreply.github.com>
2022-12-03 14:09:19 -0500

    Add script to generate survey montages.

--- stylized/survey/gen_survey.py
+++ stylized/survey/gen_survey.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 from PIL import Image, ImageDraw, ImageFont
-from os import scandir
+from os import scandir, system
 from random import choice
 
-font = ImageFont.truetype("Pillow/Tests/fonts/FreeMono.ttf", 40)
+FONT = ImageFont.truetype("Pillow/Tests/fonts/FreeMono.ttf", 50)
 
 
 def gen_survey_image(path):
@@ -24,14 +24,24 @@ def gen_survey_image(path):
             labels.remove(label)
             label_map[label] = entry.path
 
+    #store image/label map
+    print(label_map)
+    system("echo '%s' >> randomization_key.txt" % str(label_map))
+
+    #open the images
     for key in label_map.keys():
-        label_map[key] = label_img(label_map[key], key)
+        label_map[key] = Image.open(label_map[key])
 
+    #resize the images
     size = label_map["Content"].size
-
     for key in label_map.keys():
         label_map[key] = label_map[key].resize(size)
 
+    #label the images
+    for key in label_map.keys():
+        label_img(label_map[key], key)
+
+    #construct the montage
     survey_img = Image.new('RGB', (size[0] * 2, size[1] * 3))
 
     survey_img.paste(label_map["Content"], (0,       0))
@@ -40,19 +50,17 @@ def gen_survey_image(path):
     survey_img.paste(label_map["B"],       (size[0], size[1]))
     survey_img.paste(label_map["C"],       (0,       size[1] * 2))
 
-    survey_img.save("survey_%s.jpg")
+    survey_img.save("survey_%s.jpg" % path)
 
 
-def label_img(path, label):
-    img = Image.open(path)
-    ImageDraw.Draw(img).text((10, 10), label, font=FONT, fill=(0,0,0))
-    return img
+def label_img(img, label):
+    ImageDraw.Draw(img).text((10, 10), label, font=FONT, stroke_width=3, fill=(0,0,0))
 
 
-
+system("echo '' > randomization_key.txt")
 for entry in scandir():
-    if entry.is_dir:
-        gen_survey_image(entry.path)
+    if entry.is_dir():
+        gen_survey_image(entry.name)
 
 
 
